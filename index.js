402269d8bfcc3da9dd859bba6fde9f2cf6fd1d31
Amakris12 <71471809+Amakris12@users.noreply.github.com>
2021-03-31 18:19:00 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+let first = 0;
+let temp = 1;
+/*let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+let sevem = 7;
+let eight =8;
+let nine = 9;
+let ten = 10;
+let eleven = 11;
+let twelve = 12;
+let thirteen = 13;
+let fourteen = 14;
+let fifteen = 15;
+let sixteen = 16;
+let seventeen = 17;
+let eighteen = 18;
+let nineteen = 19;
+let twenty = 20;
+*/
+var input1 = prompt('Put input 1 in here');
+var input2 = prompt('Put input 2 in here');
+var numLetter = input1.split(' ');
+var ach = input2.split(' ');
+const str = 'klfbblslvjclmlnqklvg';
+const words = str.split('')
+for(i = 0; i < input1.length;i++){
+    for(j = 0; j < input2.length; j++){
+        if(input2[1])
+    }
+};
+
+
+console.log('There are'+i+"posible passwords")
